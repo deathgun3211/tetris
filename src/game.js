1c05dbc3a3028048e03a1c31cea901497899bf07
Andriy <romamur65@gmail.com>
2019-08-28 21:10:04 +0300

    'rotate'

--- src/game.js
+++ src/game.js
@@ -27,10 +27,31 @@ export default class Game {
     activePiece = {
         x: 0,
         y: 0,
-        blocks: [
+        get blocks() {
+          return this.rotations[this.rotationIndex];
+        },
+        rotationIndex: 0,
+        rotations: [
+            [
             [0,1,0],
             [1,1,1],
             [0,0,0]
+        ],
+            [
+                [0,1,0],
+                [0,1,1],
+                [0,1,0]
+            ],
+            [
+                [0,0,0],
+                [1,1,1],
+                [0,1,0]
+            ],
+            [
+                [0,1,0],
+                [1,1,0],
+                [0,1,0]
+            ]
         ]
     };
     movePieceLeft () {
@@ -59,6 +80,16 @@ export default class Game {
             this.lockPiece();
         }
 
+    }
+
+    rotatePiece() {
+        this.activePiece.rotationIndex = this.activePiece.rotationIndex < 3 ? this.activePiece.rotationIndex + 1 : 0;
+
+        if (this.isPieceOutOfBounds()) {
+            this.activePiece.rotationIndex = this.activePiece.rotationIndex > 0 ? this.activePiece.rotationIndex - 1 : 3;
+        }
+
+        return this.activePiece.blocks;
     }
 
     isPieceOutOfBounds () {
